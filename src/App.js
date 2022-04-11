a265528fe31e22faf59f04f0591d5c8439189184
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-11 12:38:08 -0300

    comenzando proyecto task manager

--- src/App.js
+++ src/App.js
@@ -2,48 +2,5 @@ import React from 'react';
 import './App.css';
 
 
-// function Helloworld(props){
-//   return(
-//     <div id="componente">
-//       <h3>{props.titulo}</h3>
-//       {props.texto}
-//     </div>
-//   )
-// }
-
-class Helloworld extends React.Component{
-  state={
-    show:true
-  }
-  render(){
-    if(this.state.show){
-      return(
-        <div id="componente">
-          <h3>{this.props.titulo}</h3>
-          {this.props.texto}
-          <button onClick={()=> this.setState({show:false})}>cambiar estado</button>
-        </div>
-      )
-    }else{
-      return(
-        <h1> no hay componente</h1>
-      )
-    }
-  }
-}
-
-
-
-
-function App() {
-  return (
-    <div>
-      Este es el componente principal
-        <Helloworld titulo="este es el titulo" texto="este es el texto"/>
-        <Helloworld titulo="este es el titulo2" texto="este es el texto 2"/>
-        <Helloworld titulo="este es el titulo" texto="este es el texto"/>
-    </div>
-  );
-}
 
 export default App;
