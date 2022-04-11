d28bf654641ebfa57592a4b9136cf8f9fbee8226
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-11 14:25:06 -0300

    establecida estructura de las tareas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {Component}from 'react';
 import './App.css';
+import tasks from "./sample/tasks.json"
+import Tasks from "./components/tasks.js";
 
+
+console.log(tasks);
+
+class App extends Component{  
+  state={
+    tasks:tasks
+  }
+  render(){
+    return(
+      <Tasks tasks={this.state.tasks}/>
+    )
+  }
+}
 
 
 export default App;
