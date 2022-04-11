b95a953aae08a2061409f225ff405ba65b1624e7
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-11 09:53:19 -0300

    pruebas con componentes y props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './App.css';
 
+
+function Helloworld(props){
+  return(
+    <div id="componente">
+      <h3>{props.titulo}</h3>
+      {props.texto}
+    </div>
+  )
+}
+
+
+
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div>
+      Este es el componente principal
+        <Helloworld titulo="este es el titulo" texto="este es el texto"/>
+        <Helloworld titulo="este es el titulo 2" texto="este es el texto 2"/>
+        <Helloworld titulo="este es el titulo 3" texto="este es el texto 3"/>
+    </div>
   );
 }
 
