dfdf974510ec2d6036613a50330bb7262732e5ac
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-11 10:55:35 -0300

    prueba cambio componentes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React from 'react';
 import './App.css';
 
 
-function Helloworld(props){
-  return(
-    <div id="componente">
-      <h3>{props.titulo}</h3>
-      {props.texto}
-    </div>
-  )
+// function Helloworld(props){
+//   return(
+//     <div id="componente">
+//       <h3>{props.titulo}</h3>
+//       {props.texto}
+//     </div>
+//   )
+// }
+
+class Helloworld extends React.Component{
+  state={
+    show:true
+  }
+  render(){
+    if(this.state.show){
+      return(
+        <div id="componente">
+          <h3>{this.props.titulo}</h3>
+          {this.props.texto}
+          <button onClick={()=> this.setState({show:false})}>cambiar estado</button>
+        </div>
+      )
+    }else{
+      return(
+        <h1> no hay componente</h1>
+      )
+    }
+  }
 }
 
 
@@ -19,8 +40,8 @@ function App() {
     <div>
       Este es el componente principal
         <Helloworld titulo="este es el titulo" texto="este es el texto"/>
-        <Helloworld titulo="este es el titulo 2" texto="este es el texto 2"/>
-        <Helloworld titulo="este es el titulo 3" texto="este es el texto 3"/>
+        <Helloworld titulo="este es el titulo2" texto="este es el texto 2"/>
+        <Helloworld titulo="este es el titulo" texto="este es el texto"/>
     </div>
   );
 }
