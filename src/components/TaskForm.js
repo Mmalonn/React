f28ad9e78569ba47b1efe7ef37f07e444bcd5509
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-12 00:48:20 -0300

    creada funcion para recoger datos del formulario

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,14 +2,46 @@ import React, {Component} from "react";
 
 export default class TaskForm extends Component{
 
+    state={
+        title:'',
+        description:'',
+    }
+
+
+    onSubmit=(e)=>{
+        console.log("hola");
+        e.preventDefault();
+    }
+
+    onChange=(e)=>{
+        console.log(
+            e.target.name,e.target.value
+        )
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+
+
+
     render(){
         return(
-            <form>
-                <input type="text" placeholder="write a task"/>
+            <form onSubmit={this.onSubmit}>
+                <input 
+                type="text"
+                name="title"
+                placeholder="write a task"
+                onChange={this.onChange}
+                value={this.state.title}/>
                 <br/>
                 <br/>
-                <textarea placeholder ="write a description"/>
-                <input type="submit"/>
+                <textarea 
+                name="description"
+                placeholder ="write a description"
+                onChange={this.onChange}
+                value={this.state.description}/>
+                <input 
+                type="submit"/>
             </form>
         )
     }
